2c0583e998bb6b6279274e3d04b1a261baa08ece
DeveloperKHJ <dbtmd1007gh1@gmail.com>
2019-03-03 17:42:33 +0900

    -

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Movie from './movie';
 
@@ -11,47 +11,43 @@ class App extends Component {
   state = { 
   }
 
+  componentDidMount(){
+    this._getMovies();
+  }
 
   _renderMovies = () =>{
-    const movies = this.state.movies.map((movieInfo, index) =>{
-      return <Movie title={movieInfo.title} poster={movieInfo.poster} key={index}/> 
-    })
-    return movies
-  }
+    const movies = this.state.movies.map( movieInfo =>{
+      return (<Movie 
+        title={movieInfo.title_english} 
+        poster={movieInfo.medium_cover_image} 
+        key={movieInfo.id} 
+        genres={movieInfo.genres}
+        synopsis={movieInfo.synopsis}
+      />
+      ); 
+    });
+    return movies;
+  };
   
-  componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "The Matrix",
-            poster: "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg"
-          },
-          {
-            title: "The Big short",
-            poster: "https://upload.wikimedia.org/wikipedia/en/1/16/The_Big_Short_%282015_film_poster%29.png"
-          },
-          {
-            title: "The Oldboy",
-            poster: "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg"
-          },
-          {
-            title: "Train to Busan",
-            poster: "https://upload.wikimedia.org/wikipedia/en/9/95/Train_to_Busan.jpg"
-          },
-          {
-            title: "Wreck-It Ralph",
-            poster: "http://pds25.egloos.com/pds/201901/12/77/b0008277_5c3947fe62ea8.jpg"
-          }
-        ]
-      })
-    }, 4000)
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    });
+  };
+ 
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    .then(Response => Response.json())
+    .then(json => json.data.movies )
+    .catch(err => console.log(err))
   }
   
   render() {
+    const {movies} = this.state;  
     return (
-      <div className="App">
-        {this.state.movies? this._renderMovies() : 'Loading'}
+      <div className={movies? "App" : "App--loading"}>
+        {movies? this._renderMovies() : 'Loading'}
       </div>
     );
   }
